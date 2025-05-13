a8b5e3870f6b5cf0fc409845cfb9dda27f1639b2
zelli17 <aayushnair321@gmail.com>
2025-05-13 16:59:46 +0530

    display sprites initial

--- src/PokemonCards.jsx
+++ src/PokemonCards.jsx
@@ -1,7 +1,7 @@
 export const PokemonCards = ({pokedata}) => {
     return <li className="pokemon-card">
         <figure>
-            
+            <img src={pokedata.sprites.other.dream_world.front_default} alt={pokedata.name} />
         </figure>
     </li>
 }
