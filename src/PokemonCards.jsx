42007af12911db1da793700624402c8a31dfe492
zelli17 <aayushnair321@gmail.com>
2025-05-13 17:27:05 +0530

    type and details given

--- src/PokemonCards.jsx
+++ src/PokemonCards.jsx
@@ -3,5 +3,44 @@ export const PokemonCards = ({pokedata}) => {
         <figure>
             <img src={pokedata.sprites.other.dream_world.front_default} alt={pokedata.name} className="pokemon-image"/>
         </figure>
+        <h1 className="pokemon-name">{pokedata.name}</h1>
+        <div className="pokemon-info pokemon-highlight">
+            <p>
+                {pokedata.types.map((curType) => curType.type.name).join(', ' )}
+            </p>
+        </div>
+
+         <div className="grid-three-cols">
+        <p className="pokemon-info">
+          <span> Height:</span> {pokedata.height}
+        </p>
+        <p className="pokemon-info">
+          <span> Weight:</span> {pokedata.weight}
+        </p>
+        <p className="pokemon-info">
+          <span> speed:</span> {pokedata.stats[5].base_stat}
+        </p>
+      </div>
+
+      <div className="grid-three-cols">
+        <div className="pokemon-info">
+          <p>{pokedata.base_experience}</p>
+          <span> Experience:</span>
+        </div>
+        <div className="pokemon-info">
+          <p>{pokedata.stats[1].base_stat}</p>
+          <span>Attack:</span>
+        </div>
+        <div className="pokemon-info">
+          <p>
+            {pokedata.abilities
+              .map((abilityInfo) => abilityInfo.ability.name)
+              .slice(0, 1)
+              .join(", ")}
+          </p>
+          <span> Abilities: </span>
+        </div>
+      </div>
+       
     </li>
 }
